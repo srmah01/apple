cb4ca7a81fcf14a3616207e25de485fb572432b5
srmah01 <srmah01@gmail.com>
2016-08-23 12:42:00 +0100

    Step 2 - Add colour, weight, taste values and hasWorm flag, with tests.

--- apple/src/main/java/org/codeslik/moneysupermarket/apple/Apple.java
+++ apple/src/main/java/org/codeslik/moneysupermarket/apple/Apple.java
@@ -4,4 +4,93 @@ package org.codeslik.moneysupermarket.apple;
  * The Apple Class.
  */
 public class Apple {
+
+	/** The colour. */
+	private String colour;
+	
+	/** The weight. */
+	private Integer weight;
+	
+	/** The taste. */
+	private Integer taste;
+	
+	/** The has worm. */
+	private Boolean hasWorm;
+
+	/**
+	 * Gets the colour.
+	 *
+	 * @return the colour
+	 */
+	public String getColour() {
+		return colour;
+	}
+
+	/**
+	 * Sets the colour.
+	 *
+	 * @param colour the new colour
+	 */
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	/**
+	 * Gets the weight.
+	 *
+	 * @return the weight
+	 */
+	public Integer getWeight() {
+		return weight;
+	}
+
+	/**
+	 * Sets the weight.
+	 *
+	 * @param weight the new weight
+	 */
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+
+	/**
+	 * Gets the taste.
+	 *
+	 * @return the taste
+	 */
+	public Integer getTaste() {
+		return taste;
+	}
+
+	/**
+	 * Sets the taste.
+	 *
+	 * @param taste the new taste value
+	 * @throws IllegalArgumentException an illegal argument exception
+	 */
+	public void setTaste(Integer taste) throws IllegalArgumentException {
+		if (taste < 1 || taste > 4) {
+			throw new IllegalArgumentException("Taste value is not within acceptable limits.");
+		}
+		this.taste = taste;
+	}
+
+	/**
+	 * Gets the checks for worm.
+	 *
+	 * @return the checks for worm
+	 */
+	public Boolean getHasWorm() {
+		return hasWorm;
+	}
+
+	/**
+	 * Sets the checks for worm.
+	 *
+	 * @param hasWorm the new checks for worm
+	 */
+	public void setHasWorm(Boolean hasWorm) {
+		this.hasWorm = hasWorm;
+	}
+	
 }
--- apple/src/test/java/org/codeslik/moneysupermarket/apple/AppleTest.java
+++ apple/src/test/java/org/codeslik/moneysupermarket/apple/AppleTest.java
@@ -28,5 +28,61 @@ public class AppleTest {
 	public void canCreate() {
 		assertNotNull(apple);
 	}
+	
+	/**
+	 * Can set colour.
+	 */
+	@Test
+	public void canSetColour() {
+		String expected = "Red";
+		apple.setColour(expected);
+		assertEquals(expected, apple.getColour());
+	}
 
+	
+	/**
+	 * Can set weight.
+	 */
+	@Test
+	public void canSetWeight() {
+		Integer expected = 100;
+		apple.setWeight(expected);
+		assertEquals(expected, apple.getWeight());
+	}
+
+	
+	/**
+	 * Can set taste.
+	 */
+	@Test
+	public void canSetTaste() {
+		Integer expected = 1;
+		apple.setTaste(expected);
+		assertEquals(expected, apple.getTaste());
+	}
+	
+	/**
+	 * Setting taste with argument below minimum throws.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void settingTasteWithArgumentBelowMinimumThrows() {
+		apple.setTaste(0);
+	}
+
+	/**
+	 * Setting taste with argument above maximum throws.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void settingTasteWithArgumentAboveMaximumThrows() {
+		apple.setTaste(5);
+	}
+	
+	/**
+	 * Can set has worm.
+	 */
+	@Test
+	public void canSetHasWorm() {
+		apple.setHasWorm(true);
+		assertTrue(apple.getHasWorm());
+	}
 }
